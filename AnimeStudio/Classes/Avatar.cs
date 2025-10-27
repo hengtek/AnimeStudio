58e948a850f6b17c12c7175abefc2dd68efac610
Manashiku <yesitsme.connrad@gmail.com>
2025-10-27 19:35:10 +0100

    Fully implement Avatar class and fix bug preventing m_TOS from being read

--- AnimeStudio/Classes/Avatar.cs
+++ AnimeStudio/Classes/Avatar.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using AnimeStudio;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace AnimeStudio
 {
@@ -70,7 +72,7 @@ namespace AnimeStudio
         public uint[] m_ID;
         public List<Axes> m_AxesArray;
 
-        public Skeleton(ObjectReader reader)
+        public Skeleton(ObjectReader reader, bool isZZZ = false)
         {
             int numNodes = reader.ReadInt32();
             m_Node = new List<Node>();
@@ -80,6 +82,7 @@ namespace AnimeStudio
             }
 
             m_ID = reader.ReadUInt32Array();
+
 
             int numAxes = reader.ReadInt32();
             m_AxesArray = new List<Axes>();
@@ -174,7 +177,14 @@ namespace AnimeStudio
         public bool m_HasRightHand;
         public bool m_HasTDoF;
 
-        public Human(ObjectReader reader)
+        // ZZZ-specific fields
+        public float m_GlobalScale;
+        public string[] m_RootMotionBoneName;
+        public bool m_HasTranslationDoF;
+        public bool m_HasExtraRoot;
+        public bool m_SkeletonHasParents;
+
+        public Human(ObjectReader reader, bool isZZZ = false)
         {
             var version = reader.version;
             m_RootX = reader.ReadXForm();
@@ -201,7 +211,6 @@ namespace AnimeStudio
             }
 
             m_HumanBoneIndex = reader.ReadInt32Array();
-
             m_HumanBoneMass = reader.ReadSingleArray();
 
             if (version[0] < 2018 || (version[0] == 2018 && version[1] < 2)) //2018.2 down
@@ -223,7 +232,9 @@ namespace AnimeStudio
             {
                 m_HasTDoF = reader.ReadBoolean();
             }
+            
             reader.AlignStream();
+
         }
     }
 
@@ -241,8 +252,9 @@ namespace AnimeStudio
         public Skeleton m_RootMotionSkeleton;
         public SkeletonPose m_RootMotionSkeletonPose;
         public int[] m_RootMotionSkeletonIndexArray;
+        public bool m_UseNextLevelForRootMotionSkeleton;
 
-        public AvatarConstant(ObjectReader reader)
+        public AvatarConstant(ObjectReader reader, bool isZZZ = false)
         {
             var version = reader.version;
             m_AvatarSkeleton = new Skeleton(reader);
@@ -251,11 +263,10 @@ namespace AnimeStudio
             if (version[0] > 4 || (version[0] == 4 && version[1] >= 3)) //4.3 and up
             {
                 m_DefaultPose = new SkeletonPose(reader);
-
                 m_SkeletonNameIDArray = reader.ReadUInt32Array();
             }
 
-            m_Human = new Human(reader);
+            m_Human = new Human(reader, isZZZ);
 
             m_HumanSkeletonIndexArray = reader.ReadInt32Array();
 
@@ -269,11 +280,114 @@ namespace AnimeStudio
 
             if (version[0] > 4 || (version[0] == 4 && version[1] >= 3)) //4.3 and up
             {
-                m_RootMotionSkeleton = new Skeleton(reader);
+                m_RootMotionSkeleton = new Skeleton(reader, isZZZ);
                 m_RootMotionSkeletonPose = new SkeletonPose(reader);
-
                 m_RootMotionSkeletonIndexArray = reader.ReadInt32Array();
             }
+
+            if (isZZZ)
+            {
+                m_UseNextLevelForRootMotionSkeleton = reader.ReadBoolean();
+            }
+        }
+    }
+
+    public class SkeletonBoneLimit
+    {
+        public Vector3 m_Min;
+        public Vector3 m_Max;
+        public Vector3 m_Value;
+        public float m_Length;
+        public bool m_Modified;
+
+        public SkeletonBoneLimit(ObjectReader reader)
+        {
+            m_Min = reader.ReadVector3();
+            m_Max = reader.ReadVector3();
+            m_Value = reader.ReadVector3();
+            m_Length = reader.ReadSingle();
+            m_Modified = reader.ReadBoolean();
+            reader.AlignStream();
+        }
+    }
+
+    public class HumanBone
+    {
+        public string m_BoneName;
+        public string m_HumanName;
+        public SkeletonBoneLimit m_Limit;
+        public HumanBone(ObjectReader reader)
+        {
+            m_BoneName = reader.ReadAlignedString();
+            m_HumanName = reader.ReadAlignedString();
+            m_Limit = new SkeletonBoneLimit(reader);
+
+        }
+    }
+
+    public class SkeletonBone
+    {
+        public string m_Name;
+        public string m_ParentName;
+        public Vector3 m_Position;
+        public Quaternion m_Rotation;
+        public Vector3 m_Scale;
+
+        public SkeletonBone(ObjectReader reader)
+        {
+            m_Name = reader.ReadAlignedString();
+            m_ParentName = reader.ReadAlignedString();
+            m_Position = reader.ReadVector3();
+            m_Rotation = reader.ReadQuaternion();
+            m_Scale = reader.ReadVector3();
+        }
+    }
+
+    public class HumanDescription
+    {
+        public List<HumanBone> m_Human;
+        public List<SkeletonBone> m_Skeleton;
+        public float m_ArmTwist;
+        public float m_ForeArmTwist;
+        public float m_UpperLegTwist;
+        public float m_LegTwist;
+        public float m_ArmStretch;
+        public float m_LegStretch;
+        public float m_FeetSpacing;
+        public float m_GlobalScale;
+        public string m_RootMotionBoneName;
+        public bool m_HasTranslationDoF;
+        public bool m_HasExtraRoot;
+        public bool m_SkeletonHasParents;
+        public HumanDescription(ObjectReader reader)
+        {
+            int numHumans = reader.ReadInt32();
+            m_Human = new List<HumanBone>(numHumans);
+            for (int i = 0; i < numHumans; i++)
+            {
+                m_Human.Add(new HumanBone(reader));
+            }
+            int numSkeleton = reader.ReadInt32();
+            m_Skeleton = new List<SkeletonBone>(numSkeleton);
+            for (int i = 0; i < numSkeleton; i++)
+            {
+                m_Skeleton.Add(new SkeletonBone(reader));
+            }
+
+
+            m_ArmTwist = reader.ReadSingle();
+            m_ForeArmTwist = reader.ReadSingle();
+            m_UpperLegTwist = reader.ReadSingle();
+            m_LegTwist = reader.ReadSingle();
+            m_ArmStretch = reader.ReadSingle();
+            m_LegStretch = reader.ReadSingle();
+            m_FeetSpacing = reader.ReadSingle();
+            m_GlobalScale = reader.ReadSingle();
+            m_RootMotionBoneName = reader.ReadAlignedString();
+            m_HasTranslationDoF = reader.ReadBoolean();
+            m_HasExtraRoot = reader.ReadBoolean();
+            m_SkeletonHasParents = reader.ReadBoolean();
+            reader.AlignStream();
         }
     }
 
@@ -283,10 +397,21 @@ namespace AnimeStudio
         public AvatarConstant m_Avatar;
         public Dictionary<uint, string> m_TOS;
 
+        public HumanDescription m_HumanDescription;
+
+        public bool IsZZZ { get; }
+
         public Avatar(ObjectReader reader) : base(reader)
         {
+
+            IsZZZ = reader.Game.Type.IsZZZ();
+
             m_AvatarSize = reader.ReadUInt32();
-            m_Avatar = new AvatarConstant(reader);
+            m_Avatar = new AvatarConstant(reader, IsZZZ);
+
+            // this is what was messing zzz up
+            // tested with multiple games, both hoyo and base unity and this align doesnt mess anything up....
+            reader.AlignStream();
 
             int numTOS = reader.ReadInt32();
             m_TOS = new Dictionary<uint, string>();
@@ -295,11 +420,16 @@ namespace AnimeStudio
                 m_TOS.Add(reader.ReadUInt32(), reader.ReadAlignedString());
             }
 
-            //HumanDescription m_HumanDescription 2019 and up
+            // finally implemented the humandescription, not particularly useful but hey one step closer to being able to export to unity ready files
+            if (reader.version[0] >= 2019)
+            {
+                m_HumanDescription = new HumanDescription(reader);
+            }
         }
 
         public string FindBonePath(uint hash)
         {
+
             m_TOS.TryGetValue(hash, out string path);
             return path;
         }
